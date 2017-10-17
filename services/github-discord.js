b6c91c019b562a246e307e6f695773a7bb8c8dd3
Pera Pisar <luka.kocka@gmail.com>
2017-10-17 20:35:36 +0200

    you sinner

--- services/github-discord.js
+++ services/github-discord.js
@@ -25,21 +25,21 @@ const USER_AGENT = 'khatch (0.0.1) http://dabpenguin.com';
 // Wikia/app config
 const REPO_ORG   = CONFIG.repo.org;
 const REPO_NAME  = CONFIG.repo.repo;
-const REPO_ID    = `${REPO_ORG}/${REPO_NAME}`;
+const REPO_ID	= `${REPO_ORG}/${REPO_NAME}`;
 const WIKIA_REPO = `https://api.github.com/repos/${REPO_ID}/events`;
 
 // Discord webhook config
-const DISCORD_WEBHOOK_ID    = CONFIG.webhook.id;
+const DISCORD_WEBHOOK_ID	= CONFIG.webhook.id;
 const DISCORD_WEBHOOK_TOKEN = CONFIG.webhook.token;
 const DISCORD_WEBHOOK_COMB  = `${DISCORD_WEBHOOK_ID}/${DISCORD_WEBHOOK_TOKEN}`;
 const DISCORD_WEBHOOK_URL   = `https://discordapp.com/api/webhooks/${DISCORD_WEBHOOK_COMB}`;
 
-const DISCORD_EMBED_COLORS = {
 // Embed colors
-    merged:   0x6E5494,
-    opened:   0x116633,
-    reopened: 0x116633,
-    closed:   0x991111
+const DISCORD_EMBED_COLORS = {
+	merged:   0x6E5494,
+	opened:   0x116633,
+	reopened: 0x116633,
+	closed:   0x991111
 };
 
 /**
@@ -60,7 +60,7 @@ const read_last = async function () {
 
 			try {
 				let last = JSON.parse(data);
-				    last.timestamp = new Date(last.timestamp);
+					last.timestamp = new Date(last.timestamp);
 
 				resolve(last);
 			} catch (e) {
@@ -110,34 +110,34 @@ const save_last = async function (id, timestamp) {
  * @return {null}
  */
 const post_to_channel = function ({title, url, user, action, avatar, user_url, body, timestamp}) {
-    const color = DISCORD_EMBED_COLORS[action];
-    if (!color) {
-        // TODO: Support more events
-        return;
-    }
+	const color = DISCORD_EMBED_COLORS[action];
+	if (!color) {
+		// TODO: Support more events
+		return;
+	}
 
 	request.post({
-        headers: {
-            'User-Agent': USER_AGENT,
-            'Content-Type': 'application/json'
-        },
-        uri: DISCORD_WEBHOOK_URL,
-        body: {
-            embeds: [{
-                title: title,
-                url: url,
-                color: color,
-                timestamp: timestamp.toISOString(),
-                description: action === 'opened' ? body : null,
-                author: {
-                    name: user,
-                    url: user_url,
-                    icon_url: avatar
-                }
-            }]
-        },
-        json: true
-    }, (err, res, body) => { });
+		headers: {
+			'User-Agent': USER_AGENT,
+			'Content-Type': 'application/json'
+		},
+		uri: DISCORD_WEBHOOK_URL,
+		body: {
+			embeds: [{
+				title: title,
+				url: url,
+				color: color,
+				timestamp: timestamp.toISOString(),
+				description: action === 'opened' ? body : null,
+				author: {
+					name: user,
+					url: user_url,
+					icon_url: avatar
+				}
+			}]
+		},
+		json: true
+	}, (err, res, body) => { });
 };
 
 /**
@@ -169,17 +169,17 @@ const fetch_actions = async function () {
 			headers: {
 				'User-Agent': USER_AGENT
 			},
-            qs: {
-                cb: new Date().getTime()
-            },
-            json: true
+			qs: {
+				cb: new Date().getTime()
+			},
+			json: true
 		};
 
 		request.get(options, function (err, res, body) {
 			if (err) reject(err);
-            if (!body) reject(new Error('No data'));
+			if (!body) reject(new Error('No data'));
 
-            resolve(body);
+			resolve(body);
 		});
 	});
 };
@@ -195,24 +195,24 @@ const filter_actions = function (actions) {
 	let n_actions = [];
 
 	for (let i = 0; i < actions.length; i++) {
-        let action = actions[i];
+		let action = actions[i];
 		if (action.type === 'PullRequestEvent') {
-            const actor   = action.actor,
-                  payload = action.payload,
-                  pr      = payload.pull_request;
-            let act = payload.action;
-            if (act === 'closed' && pr.merged_at) {
-                act = 'merged';
-            }
+			const actor   = action.actor,
+				  payload = action.payload,
+				  pr      = payload.pull_request;
+			let act = payload.action;
+			if (act === 'closed' && pr.merged_at) {
+				act = 'merged';
+			}
 			n_actions.push({
 				id:        action.id,
 				user:      actor.display_login,
-                user_url:  actor.html_url,
-                avatar:    actor.avatar_url,
-                action:    act,
+				user_url:  actor.html_url,
+				avatar:    actor.avatar_url,
+				action:    act,
 				title:     pr.title,
 				url:       pr.html_url,
-                body:      pr.body,
+				body:      pr.body,
 				timestamp: new Date(pr.updated_at)
 			});
 		}
@@ -263,8 +263,8 @@ const main = async function () {
 		if (actions.length > 0)
 			save_last(actions[0].id, actions[0].timestamp);
 	} catch (e) {
-        console.log('An error occurred:', e);
-    }
+		console.log('An error occurred:', e);
+	}
 };
 
 /**
